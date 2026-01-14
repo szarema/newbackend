f5a13df7ec5968ac91b51b876aa6aff514c1b8fc
Zarema Sirazitdinova <109747430+szarema@users.noreply.github.com>
2026-01-14 18:30:04 +0500

    debug

--- lib/handlers/events_handler.dart
+++ lib/handlers/events_handler.dart
@@ -70,5 +70,47 @@ Router eventsHandler(Connection db) {
     }
   });
 
+  /// GET EVENTS BY PET
+  router.get('/', (Request request) async {
+    try {
+      final userId = request.context['user_id'] as int;
+
+      final petIdStr = request.url.queryParameters['pet_id'];
+      final petId = int.tryParse(petIdStr ?? '');
+
+      if (petId == null) {
+        return ApiResponse.badRequest('Некорректный pet_id');
+      }
+
+      final result = await db.execute(
+        Sql.named('''
+        SELECT *
+        FROM events
+        WHERE user_id = @user_id AND pet_id = @pet_id
+        ORDER BY event_datetime ASC
+      '''),
+        parameters: {
+          'user_id': userId,
+          'pet_id': petId,
+        },
+      );
+
+      return ApiResponse.ok(
+        result.map((row) {
+          final map = row.toColumnMap();
+          // важно: сериализация даты
+          if (map['event_datetime'] is DateTime) {
+            map['event_datetime'] =
+                (map['event_datetime'] as DateTime).toIso8601String();
+          }
+          return map;
+        }).toList(),
+      );
+    } catch (e) {
+      print('❌ ERROR GET EVENTS: $e');
+      return ApiResponse.internalServerError(e);
+    }
+  });
+
   return router;
 }
